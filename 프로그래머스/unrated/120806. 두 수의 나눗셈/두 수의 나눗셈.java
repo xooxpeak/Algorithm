c4f41241651d2689256e9bcd535a34cb4ed5f74b
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2023-11-20 21:36:07 +0900

    [level unrated] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        if(0<num1 && 100>=num1 && 0<num2 && 100>=num2){
+            answer = (int)(((double)num1/num2)*1000);
+        }
+        return answer;
+    }
+}
