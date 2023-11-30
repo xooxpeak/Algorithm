82e78edc4e0123e6471f75ac2ed5fb19cb854ce8
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2023-11-30 11:09:15 +0900

    [level unrated] Title: 배열 원소의 길이, Time: 0.01 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/unrated/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -3,8 +3,8 @@ class Solution {
         // strlist배열의 길이만큼 정수 배열을 생성
         int[] answer = new int[strlist.length];
         
-        for(int i=0; i<answer.length; i++){  // 각 문자열의 길이를 정수 배열에 저장
-            answer[i] = strlist[i].length();
+        for(int i=0; i<answer.length; i++){  
+            answer[i] = strlist[i].length();  // 각 문자열의 길이를 정수 배열에 저장
         }
         return answer;
     }
