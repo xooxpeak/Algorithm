c6cfea1b330f9abe7b78409d456ea261f4a46c7a
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2023-11-30 12:09:39 +0900

    [level unrated] Title: 중복된 문자 제거, Time: 0.06 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120888. 중복된 문자 제거/중복된 문자 제거.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (char c : my_string.toCharArray()) {
+            if (answer.indexOf(String.valueOf(c)) == -1) {
+                answer.append(c);
+            }
+        }
+        
+        return answer.toString();
+    }
+}
