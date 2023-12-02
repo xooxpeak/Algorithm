9cb2da90cdb000deb73a9c745edfc0256b4711e6
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2023-12-02 11:00:16 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.12 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string, int num1, int num2) {
+        String[] s = my_string.split("");
+        
+        String tmp = "";
+        tmp = s[num1];
+        s[num1] = s[num2];
+        s[num2] = tmp;
+        
+        String answer = String.join("",s);
+        
+        return answer;
+    }
+}
