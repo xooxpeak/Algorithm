3c5f299071ccc7c82b0387e5fea0dbcde8a755cd
xooxpeak <136714432+xooxpeak@users.noreply.github.com>
2024-01-10 06:34:20 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.12 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 서울에서 김서방 찾기 - 12919 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12919?language=java) 
 
 ### 성능 요약
 
-메모리: 75.7 MB, 시간: 5.55 ms
+메모리: 77.7 MB, 시간: 0.12 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 6:19:0
+2024년 1월 3일 6:34:18
 
 ### 문제 설명
 
--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.java
@@ -1,14 +1,11 @@
-class Solution {
-    public String solution(String[] seoul) {
-        String answer = "";
+import java.util.Arrays;
+
+public class Solution {
+    public String solution(String[] seoul){
+        //x에 김서방의 위치를 저장하세요.
+        int x = Arrays.asList(seoul).indexOf("Kim");  
         
-        for(int i = 0; i < seoul.length; i++) {
-            if("Kim".equals(seoul[i])) {
-                answer = "김서방은 " + i + "에 있다";
-                break;
-            }
-         }        
+        return new StringBuilder("김서방은 ").append(x).append("에 있다").toString();
         
-        return answer;
     }
 }
