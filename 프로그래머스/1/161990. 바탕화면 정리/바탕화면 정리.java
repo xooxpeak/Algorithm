38d5a5ef2d7a5a1d90463b2776b4b8b279bba9ad
수진 <136714432+xooxpeak@users.noreply.github.com>
2024-03-09 21:12:53 +0900

    [level 1] Title: 바탕화면 정리, Time: 4.53 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,28 @@
+import java.lang.Math;
+ 
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int[] answer = new int[4];
+        
+        answer[0] = wallpaper.length;
+        answer[1] = wallpaper[0].length();
+        answer[2] = 0;
+        answer[3] = 0;
+        for (int i = 0; i < wallpaper.length; i++) {
+            String[] str = wallpaper[i].split("");
+            for (int j = 0; j < str.length; j++) {
+                if ("#".equals(str[j])) {
+                    answer[0] = Math.min(answer[0], i);
+                    answer[1] = Math.min(answer[1], j);
+                    answer[2] = Math.max(answer[2], i);
+                    answer[3] = Math.max(answer[3], j);
+                }
+            }
+        }
+        
+        answer[2] += 1;
+        answer[3] += 1;
+        
+        return answer;
+    }
+}
