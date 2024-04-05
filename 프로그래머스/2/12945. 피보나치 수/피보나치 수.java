d36d88d774e630858f1dcb51fb94d55e9076e261
수진 <136714432+xooxpeak@users.noreply.github.com>
2024-04-05 09:46:19 +0900

    [level 2] Title: 피보나치 수, Time: 4.05 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,28 +1,17 @@
+import java.math.BigInteger;
+
 class Solution {
-    // 1. 피보나치 수를 참조할 메모이제이션.
-    static int dp[] = new int[100001];
-    
-    // 2. 피보나치 메소드
-    public static int fibonacci(int n){
-        // 3. 0인 경우는 0을 반환.
-        if(n == 0) return 0;
-        // 4. 1인 경우는 1을 반환.
-        if(n == 1) return 1;
-        
-        // 5. dp[n]에 아직 값이 없다면, 아래의 식을 수행.
-        if(dp[n] == 0)
-            dp[n] = (fibonacci(n-1)+ fibonacci(n-2)) % 1234567;
-        
-        // 6. 값이 있다면 dp의 값을 참조해 바로 반환.
-        return dp[n];
-    }
-    
     public int solution(int n) {
-        int answer = 0;
+        long answer = 0L;
+        long preOne = 1L;
+        long preTwo = 1L;
         
-        // 7. 피보나치 수를 저장.
-        answer = fibonacci(n);
+        for(int i = 1; i<=n-2; i++){
+            answer = (preOne + preTwo)%1234567;
+            preOne = preTwo;
+            preTwo = answer;
+        }
         
-        return answer;
+        return (int)(answer);
     }
 }
